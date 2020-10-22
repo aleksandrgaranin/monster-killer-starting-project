1ccb28f582ec3f68990e827be365384c5309c444
aleksandGG <aleksandrgaranin89@gmail.com>
2020-10-22 13:27:22 -0700

    reset functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,43 @@
 const ATTACK_VALLUE = 10;
-const MONSTER_ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 const STONG_ATTACK_VALUE = 17;
 const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPleyerHealth = chosenMaxLife;
+let hasBonusLive = true
 
 adjustHealthBars(chosenMaxLife)
 
+function reset() {
+    currentMonsterHealth = chosenMaxLife;
+    currentPleyerHealth = chosenMaxLife;
+    resetGame(chosenMaxLife)
+}
+
 function endRound() {
-   
+    const initialPlayerHealth = currentPleyerHealth
     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPleyerHealth -= playerDamage;
+
+    if (currentPleyerHealth <= 0 && hasBonusLive) {
+        hasBonusLive = false;
+        removeBonusLife();
+        currentPleyerHealth = initialPlayerHealth
+        setPlayerHealth(initialPlayerHealth)
+        alert('You would be dead but the bonus life saved you!')
+    }
+
     if (currentMonsterHealth <= 0 && currentPleyerHealth > 0) {
         alert('Player won');
+        reset()
     } else if (currentPleyerHealth <= 0 && currentMonsterHealth > 0) {
         alert('Monster win');
+        reset()
     } else if (currentMonsterHealth <= 0 && currentPleyerHealth <= 0) {
         alert('You have a draw');
+        reset()
     }
 
 }
